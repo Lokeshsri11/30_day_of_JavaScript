98a303f77927611bf56e6b407cdb22e465d24f26
Lokesh Srivastva <lokeshsri1109@gmail.com>
2023-04-12 15:21:17 +0530

    first commit

---
+++ Introduction.js
@@ -0,0 +1,59 @@
+// Print in JavaScript
+console.log("Lokesh")
+
+
+//Arithmetics
+console.log(1 + 2)  //Addition
+console.log(2 - 1)  //Subtarction
+console.log(2 * 3)  //Multiplication
+console.log(4 / 3)  //Division
+console.log(4 % 3)  //Modulus - finding remainder
+
+
+//Code Editor
+// -> Visual Studio Code
+
+
+//String
+// 'a'
+// "Lokesh"
+// `hey how are you`
+
+
+//Booleans
+true // if the light is on, the value is true
+false // if the light is off, the value is false
+
+
+//undefined
+let firstName
+console.log(firstName)
+
+//defined
+let Name = 'loki'
+console.log(Name)
+
+//Null
+let emptyValue = null
+
+//Checking Data Types
+console.log(typeof 'Lokesh')  //string
+console.log(typeof '1')      //number
+console.log(typeof true)      // boolean
+console.log(typeof null)      // object type
+console.log(typeof undefined) // undefined
+
+
+//Variables
+//firstname, lastname, city, age, isMarried, first_name, num1, $num
+
+// Declaring different variables of different data types
+let first_Name = 'Lokesh' // first name of a person
+let lastName = 'Srivastava' // last name of a person
+let country = 'India' // country
+let city = 'Gorakhpur' // capital city
+let age = 21 // age in years
+let isMarried = false
+
+console.log(first_Name, lastName, country, city, age, isMarried)
+
